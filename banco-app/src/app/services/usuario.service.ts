b06cc8570e23c378b21f9c0d3abfeccda5129514
Alejandro512 <sebastian.guerrero02@epn.edu.ec>
2024-07-27 21:56:01 -0500

    despliegue de Back con vercel V0.1: se cambio en los servicios de Front la URL de funcionamiento

--- banco-app/src/app/services/usuario.service.ts
+++ banco-app/src/app/services/usuario.service.ts
@@ -7,7 +7,8 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class UsuarioService {
-  private baseUrl = 'http://localhost:4000/api'; 
+  //private baseUrl = 'http://localhost:4000/api'; 
+  private baseUrl = 'https://base-datos-api-rest.vercel.app/api'; 
 
   constructor(private http: HttpClient) { }
 
